9bfd2b37df85c5515d1b19c6079ebd9b377cd826
sandy-1207 <107939605+sandy-1207@users.noreply.github.com>
2022-09-13 16:12:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAU2BCLYCTZ3ZRTD6H"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
