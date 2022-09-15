1597e8ebcb4df41bfa4139b7ee5c9416b56cad82
sandy-1207 <107939605+sandy-1207@users.noreply.github.com>
2022-09-15 17:14:42 +0530

    Update lb.tf

--- lb.tf
+++ lb.tf
@@ -10,7 +10,7 @@ resource "aws_lb_target_group" "external-elb" {
   name     = "ALB-TG"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.demovpc.id
+  vpc_id   = aws_vpc.tf-vpc.id
 }
 
 resource "aws_lb_target_group_attachment" "external-elb1" {
